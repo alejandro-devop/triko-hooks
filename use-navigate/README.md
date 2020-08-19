118ddb7dcfc460688d3f02398dd7c44104bf2830
Alejandro Quiroz <alejandro.devop@gmail.com>
2020-08-19 01:41:42 -0500

    useNavigate
    Created a hook to access the navigation functions.

---
+++ use-navigate/README.md
@@ -0,0 +1,8 @@
+# useNavigate
+this hook allows to access the react navigation functions, returns the following object: 
+```javascript
+{
+  navigation: Object,
+  redirectTo: func(path, params),  
+}
+```
---
+++ use-navigate/index.js
@@ -0,0 +1,40 @@
+import {useNavigation, StackActions} from '@react-navigation/native';
+
+// const prepareStack = (paths = []) => {
+//   const actions = paths.map(path =>
+//     NavigationActions.navigate({routeName: path}),
+//   );
+//   const index = actions.length - 1;
+//   return {index, actions};
+// };
+
+/**
+ * This hook allows access to the navigation and re-directions
+ * @author Jorge Alejandro Quiroz Serna <jakop.box@gmail.com>
+ * @version 1.0.0
+ * @app Client, Triko
+ * @returns {{redirectWith: redirectWith, redirectTo: redirectTo}}
+ */
+const useNavigate = () => {
+  const navigation = useNavigation();
+  return {
+    navigation,
+    redirectTo: (path, params) => {
+      navigation.dispatch(StackActions.replace(path, params));
+      // const {initial} = options;
+      // let paths = [path];
+      // if (initial) {
+      //   paths = [initial, path];
+      // }
+      // navigation.dispatch(StackActions.reset(prepareStack(paths)));
+    },
+    redirectWith: (paths = []) => {
+      // navigation.dispatch(StackActions.reset(paths));
+    },
+    reset: () => {
+
+    },
+  };
+};
+
+export default useNavigate;
