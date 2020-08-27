d1ee9f45badbeb9e1a41f61bd7985e850de11575
Alejandro Quiroz <alejandro.devop@gmail.com>
2020-08-27 15:26:39 -0500

    useKeyboard
    Added hook to detect key visibility

---
+++ use-keyboard/index.js
@@ -0,0 +1,31 @@
+import {useEffect, useState} from 'react';
+import {Keyboard} from 'react-native';
+
+export const EVENT__DID_SHOW = 'keyboardDidShow';
+export const EVENT__DID_HIDE = 'keyboardDidHide';
+
+const useKeyboard = () => {
+  const [keyboardVisible, setVisible] = useState(false);
+  const [keyBoardHeight, setKeyboardHeight] = useState(0);
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(EVENT__DID_SHOW, e => {
+      const {endCoordinates = {}} = e;
+      setKeyboardHeight(endCoordinates.height);
+      setVisible(true);
+    });
+    const keyboardDidHideListener = Keyboard.addListener(
+      EVENT__DID_HIDE,
+      () => {
+        setVisible(false);
+      },
+    );
+
+    return () => {
+      keyboardDidHideListener.remove();
+      keyboardDidShowListener.remove();
+    };
+  }, []);
+  return [keyboardVisible, keyBoardHeight];
+};
+
+export default useKeyboard;
