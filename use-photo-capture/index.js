a17976a12df0d00e5742ee56908f3216403c2c2f
Alejandro Quiroz <alejandro.devop@gmail.com>
2020-09-13 13:55:13 -0500

    added photo capture hook

---
+++ use-photo-capture/index.js
@@ -0,0 +1,36 @@
+import useTranslation from 'hooks/useTranslation';
+import ImagePicker from 'react-native-image-picker';
+
+const usePhotoCapture = (otherOptions = {}) => {
+  const {_t} = useTranslation();
+  const options = {
+    title: _t('select_an_image_from_gallery'),
+    cancelButtonTitle: _t('image_picker_cancel'),
+    takePhotoButtonTitle: _t('image_picker_take_photo'),
+    chooseFromLibraryButtonTitle: _t('image_picker_from_gallery'),
+    maxWidth: 500,
+    maxHeight: 500,
+    quality: 0.6,
+    storageOptions: {
+      skipBackup: true,
+      path: 'images',
+    },
+    ...otherOptions,
+  };
+  return (config = {}) => {
+    const {onCustom, onPhotoSelected, onCancel, onError} = config;
+    ImagePicker.showImagePicker(options, response => {
+      if (response.didCancel && onCancel) {
+        onCancel(response.didCancel);
+      } else if (response.error && onError) {
+        onError(response.error);
+      } else if (response.customButton && onCustom) {
+        onCustom(response.customButton);
+      } else if (onPhotoSelected) {
+        onPhotoSelected(response);
+      }
+    });
+  };
+};
+
+export default usePhotoCapture;
