084245ee83379f8725b31c0d4ba832203275aca1
Alejandro Quiroz <alejandro.devop@gmail.com>
2020-11-10 02:58:06 -0500

    Added error report to the user current location hook

--- use-user-location/index.js
+++ use-user-location/index.js
@@ -1,15 +1,28 @@
 import {useEffect, useState} from 'react';
 import useNotify from 'hooks/useNotification';
 import Geolocation from '@react-native-community/geolocation';
-// Geolocation.setRNConfiguration({
-//   skipPermissionRequests: true,
-// });
+import useErrorReporter from 'shared/hooks/use-error-reporter';
 
+/**
+ * This hook gets the user current location
+ * @version 1.0.0
+ * @author Alejandro <alejandro.devop@gmail.com>
+ * @param options
+ * @returns {{getLocation: *, location: *, loading: *}}
+ */
 const useUserLocation = (options = {}) => {
   const {onLocationFound} = options;
   const [loading, setLoading] = useState(false);
+  const reportError = useErrorReporter({
+    path: 'src/shared/hooks/use-user-location/index.js',
+  });
   const [location, setLocation] = useState({});
   const {error} = useNotify();
+
+  /**
+   * This function is used to fetch the user current location.
+   * @returns {Promise<void>}
+   */
   const getLocation = async () => {
     setLoading(true);
     try {
@@ -31,6 +44,7 @@ const useUserLocation = (options = {}) => {
       }
       setLoading(false);
     } catch (e) {
+      reportError(e);
       error('Error while getting the current location');
       setLoading(false);
     }
