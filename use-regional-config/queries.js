15956f24e2d37a64fd810eacddabc0f72d7fe7a4
Alejandro Quiroz <alejandro.devop@gmail.com>
2020-11-28 19:17:07 -0500

    Auto stash before rebase of "origin/master"

--- use-regional-config/queries.js
+++ use-regional-config/queries.js
@@ -3,6 +3,14 @@ import gql from 'graphql-tag';
 export const GET_REGION_CONFIG = gql`
   query getRegionConfig($region: Int) {
     response: configs(region_id: $region) {
+      applications {
+        triko: trikowork {
+          version
+        }
+        client: trikoclient {
+          version
+        }
+      }
       emails {
         backgroundCheck
       }
