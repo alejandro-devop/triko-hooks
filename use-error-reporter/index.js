1292d58056c76ec788d75dd0f9a96812c5f62aaa
Alejandro <alejandro.devop@gmail.com>
2020-11-30 14:34:03 -0500

    Added functionality to error reporter

--- use-error-reporter/index.js
+++ use-error-reporter/index.js
@@ -1,7 +1,43 @@
+import {APP_CODE, ERROR_REPORTER_URL} from 'react-native-dotenv';
+import {useSession} from 'hooks/index';
+import {isEmpty} from 'shared/utils/functions';
+import useNotify from 'hooks/useNotification';
+import useTranslation from 'hooks/useTranslation';
+
 const useErrorReporter = (config = {}) => {
-  const {componentPath, path} = config;
-  return (errorInfo = {}) => {
-    console.log('error: ', errorInfo);
+  const {
+    stack: {user = {}},
+  } = useSession();
+  const {_t} = useTranslation();
+  const {error} = useNotify();
+  const userId = !isEmpty(user) ? user.id : 'none';
+  const phone = !isEmpty(user) ? user.phonenumber : 'none';
+
+  const {path} = config;
+  return async (errorInfo = {}, messageOptions = {}) => {
+    const {message, code} = messageOptions;
+    try {
+      if (message) {
+        error(_t(message, {code}));
+      }
+      await fetch(ERROR_REPORTER_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          application: APP_CODE,
+          file: path,
+          error: errorInfo.message,
+          user: userId,
+          phone: phone,
+          code,
+        }),
+      });
+      // response.json();
+    } catch (e) {
+      console.log('Cannot get error');
+    }
   };
 };
 
